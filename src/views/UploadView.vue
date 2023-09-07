8fffbed454590516b21eed17e3a237ecda948465
Nattapong Jitrangsi <nattapong.jir@student.mahidol.edu>
2023-09-07 21:17:56 +0700

    complete to add all component  in upload page

--- src/views/UploadView.vue
+++ src/views/UploadView.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="upload-view">
     <header class="logo-header">
-      <img src="@/assets/logo.png" alt="Logo" />
+      <font-awesome-icon
+        class="fa-icon"
+        icon="folder-plus"
+        :style="{ color: '#62aa92' }"
+      />
       <h1>Import your File or Folder</h1>
     </header>
     <div class="content">
@@ -10,18 +14,22 @@
           <p class="drop-message-text">
             <span class="bold-text">Drop your file or Folder here</span>
           </p>
-          <p>
-            <img src="@/assets/logo.png" alt="File Icon" class="file-icon" />
+          <p class="p-margin">
+            <font-awesome-icon
+              :icon="['far', 'file-image']"
+              :style="{ color: '#d6d6d6' }"
+              size="6x"
+            />
           </p>
-          <p>
+          <p class="p-margin">
             <span class="gray-text">and </span>
             <span class="underline">other similar</span>
             <span class="gray-text"> file types</span>
           </p>
-          <p>
-            <span class="gray-text-or">-or-</span>
+          <p class="p-margin">
+            <span class="or-line">or</span>
           </p>
-          <p>
+          <p class="p-margin">
             <input
               type="button"
               value="Import File or Folder.."
@@ -35,88 +43,5 @@
 </template>
 
 <style scoped>
-.upload-view {
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  height: 100vh;
-  padding: 20px;
-  box-sizing: border-box;
-}
-
-.logo-header {
-  display: flex;
-  align-items: center;
-  justify-content: flex-start; /* จัดชิดซ้าย */
-  margin-bottom: 20px;
-  margin-left: 100px; /* เพิ่ม margin left 50px */
-}
-
-.logo-header img {
-  width: 50px;
-  height: 50px;
-  margin-right: 10px;
-}
-
-.content {
-  width: 100%;
-  margin-top: 20px;
-  display: flex;
-  justify-content: center;
-}
-
-.drop-area {
-  width: 800px; /* กำหนดความกว้างเป็น 800px */
-  margin: 0 auto; /* จัดให้อยู่กึ่งกลางหน้าจอ */
-  height: 500px;
-  border: 2px dashed #ccc;
-  border-radius: 10px;
-  padding: 20px;
-  text-align: center;
-  cursor: pointer;
-}
-
-.drop-message-text {
-  font-size: 16px;
-  margin-bottom: 10px;
-}
-
-.bold-text {
-  font-weight: bold;
-  color: black;
-  /* ตัวหนาสีดำ */
-}
-
-.file-icon {
-  width: 100px;
-  height: 100px;
-  margin: 0 10px;
-}
-
-.gray-text {
-  color: #aaa;
-  /* สีเทาอ่อน */
-}
-
-.gray-text-or {
-  font-weight: bold;
-  color: #aaa;
-  /* สีเทาอ่อน */
-}
-
-.underline {
-  color: #aaa;
-  text-decoration: underline;
-}
-
-.import-button {
-  background-color: #007bff;
-  color: white;
-  border: none;
-  padding: 10px 20px;
-  border-radius: 5px;
-  cursor: pointer;
-  font-weight: bold;
-  margin-top: 20px;
-}
+@import "../assets/styles/UploadView.css";
 </style>

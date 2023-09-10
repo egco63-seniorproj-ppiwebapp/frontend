583ccbf7c9fee0c0d52c581f86b53fc4a1538487
Nattapong Jitrangsi <nattapong.jir@student.mahidol.edu>
2023-09-10 17:37:17 +0700

    complete to create modal component

--- src/views/UploadView.vue
+++ src/views/UploadView.vue
@@ -1,5 +1,38 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <ModalComponent
+      :isVisible="isModalVisible"
+      headerText="Upload failed ❗️"
+      descriptionText="There was an issue uploading your image. Please try again."
+      @close="handleModalClose"
+    />
   </div>
 </template>
+
+<script>
+import ModalComponent from "../components/ModalComponent.vue";
+
+export default {
+  components: {
+    ModalComponent,
+  },
+  data() {
+    return {
+      isModalVisible: true,
+    };
+  },
+  methods: {
+    async uploadImage() {
+      try {
+        // โค้ดสำหรับอัพโหลดรูปภาพ
+      } catch (error) {
+        // ถ้ามีข้อผิดพลาดในการอัพโหลด
+        this.isModalVisible = true; // แสดง modal
+      }
+    },
+    handleModalClose() {
+      this.isModalVisible = false;
+    },
+  },
+};
+</script>

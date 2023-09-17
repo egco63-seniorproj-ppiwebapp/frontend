003c423318c2a2fb4ff4b7bc930841feec41789d
Nattapong Jitrangsi <nattapong.jir@student.mahidol.edu>
2023-09-17 21:13:23 +0700

    create script to upload img

--- src/views/UploadView.vue
+++ src/views/UploadView.vue
@@ -11,7 +11,7 @@
     <div class="content">
       <div class="side-space"></div>
       <div class="drop-area" @dragover.prevent @drop="handleFileDrop">
-        <div class="drop-message">
+        <div class="drop-message" v-if="!isUploading">
           <p class="drop-message-text">
             <span class="bold-text">Drop your file or Folder here</span>
           </p>
@@ -32,18 +32,112 @@
           </p>
           <p class="p-margin">
             <input
+              type="file"
+              ref="fileInput"
+              @change="uploadFile"
+              style="display: none"
+              accept=".jpeg,.jpg,.png"
+            />
+            <input
               type="button"
               value="Import File or Folder.."
               class="primary"
+              @click="triggerFileInput"
             />
           </p>
+        </div>
+        <div class="drop-message" v-if="isUploading">
+          <div class="upload-section">
+            <p class="upload-text">Uploading...</p>
+            <div
+              v-for="file in uploadingFiles"
+              :key="file.name"
+              class="file-upload-container"
+            >
+              <span class="file-name">{{ file.name }}</span>
+              <div class="progress-container">
+                <div
+                  :style="{ width: file.progress + '%' }"
+                  class="progress-bar"
+                ></div>
+              </div>
+              <span class="cancel-icon" @click="cancelUpload(file)">x</span>
+            </div>
+          </div>
         </div>
       </div>
       <div class="side-space"></div>
     </div>
-    <footer class="footer"></footer>
+    <footer class="footer">
+      <div v-if="responseMessage" class="response-message">
+        {{ responseMessage }},
+        {{ isUploading }}
+      </div>
+    </footer>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      responseMessage: "",
+      isUploading: false, // ตัวแปรสำหรับควบคุมการแสดง/ซ่อนส่วนต่างๆ
+      uploadingFiles: [], // สำหรับเก็บไฟล์ที่กำลังอัพโหลด
+    };
+  },
+  methods: {
+    triggerFileInput() {
+      this.$refs.fileInput.click();
+    },
+    async uploadFile(event) {
+      this.isUploading = true;
+      const file = event.target.files[0];
+      this.uploadingFiles.push({ name: file.name, progress: 0 });
+
+      let formData = new FormData();
+      formData.append("file", file);
+
+      try {
+        let response = await fetch("http://127.0.0.1:8000/upload", {
+          method: "POST",
+          body: formData,
+        });
+
+        if (response.ok) {
+          let result = await response.json();
+          this.responseMessage = result.message;
+          const fileIndex = this.uploadingFiles.findIndex(
+            (f) => f.name === file.name
+          );
+          if (fileIndex !== -1) {
+            this.uploadingFiles[fileIndex].progress = 100;
+            setTimeout(() => {
+              this.uploadingFiles.splice(fileIndex, 1);
+            }, 1000);
+          }
+        } else {
+          console.error("File upload failed");
+          this.responseMessage = "File upload failed";
+        }
+      } catch (error) {
+        console.error("There was an error uploading the file.", error);
+        this.responseMessage = error.message || "Unknown error occurred.";
+      } finally {
+        // this.isUploading = false;
+      }
+    },
+    cancelUpload(file) {
+      const fileIndex = this.uploadingFiles.findIndex(
+        (f) => f.name === file.name
+      );
+      if (fileIndex !== -1) {
+        this.uploadingFiles.splice(fileIndex, 1);
+      }
+    },
+  },
+};
+</script>
 
 <style scoped>
 @import "../assets/styles/UploadView.css";

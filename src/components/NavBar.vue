92ab4bb4ea5d99943cce69fc0b40e41b1ab5d896
piyawat-wir <77828805+piyawat-wir@users.noreply.github.com>
2023-09-25 02:24:57 +0700

    Improve and integrate userprofile  at NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -8,45 +8,70 @@
     </div>
 
     <div class="user-profile">
-      <span class="user-name">Admin123</span>
-      <font-awesome-icon
-        :icon="['fas', 'arrow-right-from-bracket']"
-        style="color: #ffffff"
-        @click="logOut()"
-      />
+      <span class="user-name">{{ username }}</span>
+      <button class="logout-btn" @click="logOut()">
+        <font-awesome-icon :icon="['fas', 'arrow-right-from-bracket']" />
+      </button>
     </div>
   </nav>
 </template>
 
 <script lang="ts">
+import router from "@/router";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "NavBar",
+  computed: {
+    username() {
+      return this.$store.getters.username;
+    },
+  },
   methods: {
     logOut() {
-      console.log("pls implement function to me");
+      this.$store.dispatch("logout");
+      router.push("/login");
     },
   },
 });
 </script>
 
-<style scoped>
-nav {
-  display: flex;
-  justify-content: space-between;
-}
+<style lang="sass" scoped>
+nav
+  display: flex
+  justify-content: space-between
 
-.user-profile {
-  display: flex;
-  align-items: center;
-  margin-bottom: 10px;
-}
 
-.user-name {
-  font-weight: 400;
-  font-size: 16px;
-  color: #ffffff;
-  margin-right: 10px;
-}
+.user-profile
+  display: flex
+  align-items: center
+  position: relative
+  right: -30px
+  top: -5px
+
+
+.user-name
+  font-weight: 400
+  font-size: 16px
+  color: #ffffff
+  margin-right: 12px
+  padding-right: 12px
+  border-right: 1px solid rgba(255, 255, 255, 0.5)
+
+
+.logout-btn
+  width: 40px
+  height: 40px
+  cursor: pointer
+  display: flex
+  justify-content: center
+  align-items: center
+  color: white
+  border-radius: 50%
+  background: none
+  outline-color: rgba(255, 255, 255, 0.2)
+
+  &:hover
+    background: rgba(255, 255, 255,0.2)
+    box-shadow: 0 0 24px -5px rgba(0, 0, 0, 0.04)
 </style>

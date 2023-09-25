30d9602dc291af0fe270a393e49997e50479754b
piyawat-wir <77828805+piyawat-wir@users.noreply.github.com>
2023-09-26 04:04:46 +0700

    Add Empty Gallery div

--- src/views/ImagesView.vue
+++ src/views/ImagesView.vue
@@ -4,8 +4,20 @@
       <ImageSearch :search-handler="search" />
     </div>
     <div class="gallery">
-      <ImageGrid :images="images" v-if="renderGallery" />
-      <LoadSpinner v-else />
+      <LoadSpinner v-if="!renderGallery" />
+      <ImageGrid :images="images" v-else-if="images.length > 0" />
+      <div class="empty-gallery" v-else>
+        <span style="font-size: 100px"
+          ><font-awesome-icon :icon="['fas', 'wind']"
+        /></span>
+        <h2>Gallery is empty.</h2>
+        <p>Try adding more images.</p>
+        <router-link to="/upload">
+          <button class="primary outline">
+            <font-awesome-icon :icon="['fas', 'fa-file-upload']" /> Upload
+          </button></router-link
+        >
+      </div>
     </div>
   </div>
   <router-view />
@@ -23,6 +35,13 @@
   height: calc(100vh - navbar.$navbar-height)
   overflow-y: scroll
 
+.empty-gallery
+  height: 100%
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center
+
 .search-panel
   padding: 1.5rem
   height: calc(100vh - navbar.$navbar-height)
@@ -33,7 +52,7 @@ import { defineComponent } from "vue";
 import ImageGrid from "@/components/ImageGrid.vue";
 import ImageSearch from "@/components/ImageSearch.vue";
 import { SearchParameters, ImageThumbnailData, ImageMetadata } from "@/types";
-import { parseImageMetadata } from "@/utils";
+import { handleAxiosResponse, parseImageMetadata } from "@/utils";
 
 import axios from "axios";
 import LoadSpinner from "@/components/LoadSpinner.vue";
@@ -103,9 +122,11 @@ export default defineComponent({
       return validParams;
     },
     async loadImages() {
-      let res = await axios.get("/api/get_collection", {
-        params: this.serializeSearchParams(),
-      });
+      let res = await handleAxiosResponse(() =>
+        axios.get("/api/get_collection", {
+          params: this.serializeSearchParams(),
+        })
+      );
       if (res.status != 200) return [] as Array<ImageThumbnailData>;
       // console.log(res.data);
       const imgMetaList = res.data as Array<ImageMetadata>;
